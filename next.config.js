018ee68852b39f2d8e65223384454379abfe8c76
jdsvsn <judeparu123@gmail.com>
2026-04-09 12:55:12 +0800

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: '/haha-1775710497351',
+  assetPrefix: '/haha-1775710497351',
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+};
+module.exports = nextConfig;
